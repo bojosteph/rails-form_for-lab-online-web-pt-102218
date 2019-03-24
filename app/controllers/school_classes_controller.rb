56b874cf2876ef8f26e4b51af4208642b349e5e6
Roberto Samia <bojosteph@gmail.com>
2019-03-24 03:56:03 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,6 +1,27 @@
 class SchoolClassesController < ApplicationController
   
+  def new 
+    @school_class = SchoolClass.new
+  end 
+  
+  def create 
+  end 
+  
+  def show 
+  end 
+  
+  def edit 
+  end 
+  
+  def update 
+  end 
+  
+  
+  
+  
+  
 private
+
 def school_class_params(*args)
   params.require(:school_class).permit(*args)
 end
