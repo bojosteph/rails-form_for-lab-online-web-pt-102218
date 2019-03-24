47fc50f19de5fd320dd60cf3ae5c8abbb392f317
Roberto Samia <bojosteph@gmail.com>
2019-03-24 04:00:02 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,7 +6,8 @@ class SchoolClassesController < ApplicationController
   
   def create 
     @school_class = SchoolClass.new(school_class_params(:title, :room_number))
-    
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end 
   
   def show 
@@ -18,6 +19,8 @@ class SchoolClassesController < ApplicationController
   end 
   
   def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params())
     
   end 
   
